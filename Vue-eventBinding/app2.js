ff6bcc71497f98d0f84977044227cc690a625b06
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-12 16:25:43 +0530

    Update app2.js

--- Vue-eventBinding/app2.js
+++ Vue-eventBinding/app2.js
@@ -2,14 +2,29 @@ const app2 = Vue.createApp({
     data() {
         return {
             count: 0,
-            name: 'Karan',
+            name: '',
+            lastName: '',
             confirmedName: '',
         };
 
     },
+    watch: {
+        count(value) {
+            if (value > 5){
+                this.count = 0;
+            }
+        },
+    //    name() {
+    //        this.fullName = this.name + this.lastName;
+    //    },
+    //    lastName() {
+    //        this.fullName = this.name + this.lastName;
+
+    //    }
+    },
     computed: {
         fullName(){
-            return this.name + ' Sonawane';
+            return this.name + this.lastName;
         },
     },
     methods: {
