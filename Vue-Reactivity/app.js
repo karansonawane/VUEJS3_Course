10daf9ce7d5b9237b8d44786e7658a4390565e4f
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-13 11:26:04 +0530

    Vue-reactivity Uploaded

---
+++ Vue-Reactivity/app.js
@@ -0,0 +1,35 @@
+
+// **********First App**********
+
+const commonData ={
+    commonMessage: 'This is the common message',
+};
+
+const app = Vue.createApp({
+    data () {
+        return {
+            message: 'Hello World',
+            data: commonData,
+        };
+    }
+});
+
+app.mount('#app');
+
+// **********Second App********** 
+
+const app2 = Vue.createApp({
+    data() {
+        return {
+            anotherMessage: 'Hello Another World',
+            data: commonData,
+        };
+    },
+    methods: {
+        changeMessage(){
+            this.data.commonMessage = 'Changed The Message';
+        },
+    },
+});
+
+app2.mount('#app2');
