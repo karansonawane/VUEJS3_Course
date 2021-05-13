26a71aa444eb35120ba8c17f22ca6026d7a53f35
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-13 22:01:00 +0530

    uploaded SinglePost vue File

---
+++ vue3-project/src/components/SinglePost.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="title">{{post.title}}</div>
+        <div>{{post.description}}</div>
+        <a href="" @click.prevent="changeTitle()">Click to Change the title</a>
+    </div>
+</template>
+<script>
+export default {
+    props: ['data'],
+    data() {
+        return{
+            post: this.data,
+        };
+    },
+    methods: {
+        changeTitle(){
+            this.post.title = 'Changed The Title';
+        }
+    }
+};
+</script>
+<style>
+.title{
+    color: red;
+    font-weight: bold;
+}
+</style>
