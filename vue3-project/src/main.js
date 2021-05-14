38c93f6df7325a562229a6a515d9a06db971c8aa
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-14 10:36:34 +0530

    Update main.js

--- vue3-project/src/main.js
+++ vue3-project/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import SinglePost from './components/SinglePost.vue';
+import Lecture from './components/Lecture.vue';
 
 const app = createApp(App)
 
 app.component('SinglePost', SinglePost);
+app.component('Lecture', Lecture);
 app.mount('#app')
