c1fdfe5e0f0c5c0251322979e9443a8f8dd629c8
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-14 10:35:23 +0530

    Lecture vue file uploaded

---
+++ vue3-project/src/components/Lecture.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="title">{{lecture.title}}</div>
+    <div>{{lecture.description}}</div>
+    <a href="" @click.prevent="ChangeLecTitle()">Click to change Title</a>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['data'],
+  data() {
+    return {
+        lecture: this.data,
+    };
+  },
+  methods: {
+      ChangeLecTitle() {
+          this.lecture.title = 'Changed The Title';
+      },
+  },
+};
+</script>
+<style>
+.title{
+    color: red;
+    font-weight: bold;
+}
+</style>
