2a71c9a394274dadc1ff72d4a7de4b7a1b5fe3a6
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-14 21:52:02 +0530

    Update App.vue

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-8">
+        <div class="my-4">
+          <select-component
+            data-status="1"
+            id="selectbox"
+            @change="selectChange"
+          ></select-component>
+        </div>
+        <a href="" @click.prevent="changeTitle()">Click to change title</a>
         <h2>Post</h2>
         <div v-for="post in posts" :key="post.title">
-          <single-post :data="post"></single-post>
+          <single-post :data="post" :isactive="1"></single-post>
           <hr />
         </div>
         <h2>Lecture</h2>
         <div v-for="lecture in lectures" :key="lecture.title">
-          <lecture :data="lecture"></lecture>
+          <lecture :data="lecture" :isactive="1"></lecture>
           <hr />
         </div>
+      </div>
+      <div class="col-md-4 my-4">
+        <h3>Latest Post</h3>
+        <ul class="list-group">
+          <li class="list-group-item" v-for="post in posts" :key="post.title">{{post.title}}</li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Lecture from './components/Lecture.vue';
+import Lecture from "./components/Lecture.vue";
+import SelectComponent from "./components/SelectComponent.vue";
 export default {
   data() {
     return {
-      lectures :[
+      lectures: [
         {
           title: "Lecture 1",
           description: "Lecture One Discription",
@@ -44,8 +59,16 @@ export default {
       ],
     };
   },
+  methods: {
+    selectChange(event) {
+      console.log(event.target.value);
+    },
+  },
   name: "App",
-  components: {Lecture},
+  components: {
+    Lecture,
+    SelectComponent,
+  },
 };
 </script>
 
