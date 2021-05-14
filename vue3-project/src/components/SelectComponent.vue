32c4d04dd1b9fa576b552a1ed679dd7fdb05e219
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-14 21:51:09 +0530

    SelectComponent uploaded

---
+++ vue3-project/src/components/SelectComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <select>
+            <option value="">Select</option>
+            <option value="1">1</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+}
+</script>
