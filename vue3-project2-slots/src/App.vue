012718cfed89626da9dbfd66cfd6ee1966faebf8
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-19 12:52:02 +0530

    Added new Vue app named "vue3-project2-slots"

---
+++ vue3-project2-slots/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+
+  <Button> <em class="fa fa-caret-down"></em>&nbsp; User Profile </Button>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <profile-card>
+          <template v-slot:header>
+            <div>Header Title</div>
+          </template>
+
+          <template v-slot:content>
+            <div>Content</div>
+            <div>content message</div>
+          </template>
+
+          <template v-slot:footer>
+            <div>Footer Message</div>
+          </template>
+        </profile-card>
+      </div>
+    </div>
+
+    <div class="container my-4">
+      <div class="row">
+        <div class="col-md-4">
+          <hobbies  #default="slotProps">
+              <h2>{{slotProps.hobby}}</h2>
+          </hobbies>
+        </div>
+        <div class="col-md-4">
+          <hobbies>
+            <template #default="slotProps">
+              <div>{{slotProps.hobby}}</div>
+            </template>
+          </hobbies>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Button from "./components/Button.vue";
+import ProfileCard from "./components/ProfileCard.vue";
+import Hobbies from "./components/Hobbies.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Button,
+    ProfileCard,
+    Hobbies,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3-project2-slots/src/components/ProfileCard.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <div class="header">
+        <slot name="header"></slot>
+    </div>
+    <div class="content">
+        <slot name="content"></slot>
+    </div>
+    <div class="footer">
+        <slot name="footer"></slot>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style scoped>
+.card {
+    border: 3px solid #eee;
+    box-shadow: 3px 3px 3px #eee;
+}
+.header{
+    background: #eee;
+    font-size: 20px;
+}
+.content{
+    padding: 20px 10px;
+}
+.footer{
+    background: #eee;
+    font-size: 20px;
+}
+</style>
