9a259ef47e0551bfcc197959063c43a4eeea3410
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-21 11:11:17 +0530

    Update main.js

--- project4-vue-http/src/main.js
+++ project4-vue-http/src/main.js
@@ -1,8 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Axios from 'axios'
+import { createApp } from 'vue';
+import App from './App.vue';
+import Axios from 'axios';
 
 Axios.defaults.headers.common['Karan'] = 'Karan Sonawane config';
 Axios.defaults.baseURL = 'https://vue-course-9cc29-default-rtdb.firebaseio.com/';
+
+// Add a request interceptor
+Axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    config.headers.common['interceptorheader'] = 'Interceptor Header'
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+  // Add a response interceptor
+Axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    response.data = { hai: 'hai' };
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  });
 
 createApp(App).mount('#app')
