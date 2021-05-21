5a3271c432794d0aac1e6cd0574aee1eba8068e0
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-21 15:45:00 +0530

    Update Home.vue

--- vue3-project5-vue-routing/src/components/pages/Home.vue
+++ vue3-project5-vue-routing/src/components/pages/Home.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
-        Home Page
+        <div>
+            <button @click.prevent="onPostsClick()">Click to post page</button>
+        </div>
+        <div> Home Page </div>
     </div>
 </template>
 
 <script>
 export default {
-    
-}
+    methods: {
+        onPostsClick() {
+            this.$router.push({ path: '/posts', query: { id: 1 } });
+        },
+    },
+};
 </script>
