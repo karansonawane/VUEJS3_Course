24ef31e825b41f153b8aa819c0a86f9a022279bb
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-24 19:49:13 +0530

    Update PostItem.vue

--- vue3-project5-vue-routing/src/components/pages/PostItem.vue
+++ vue3-project5-vue-routing/src/components/pages/PostItem.vue
@@ -7,7 +7,12 @@
       <div>Description: {{ post.description }}</div>
     </div>
     <div>
-        <router-link class="btn btn-dark" to="/posts">Back</router-link>
+      <router-link to="/posts/-MaSFIo6O84vdh66Ab-j"
+        >go- to post id -MaSFIo6O84vdh66Ab-j</router-link
+      >
+    </div>
+    <div>
+      <router-link class="btn btn-dark" to="/posts">Back to Posts</router-link>
     </div>
   </div>
 </template>
@@ -21,11 +26,31 @@ export default {
       id: "",
     };
   },
+
+  // ************* Second Method **************
+
+  watch: {             
+    $route() {
+      console.log('watch called');
+      this.getPostDetails();
+    },
+  },
   created() {
-    this.id = this.$route.params.id;
-    axios.get(`posts.json?id=${this.id}`).then((response) => {
-      this.post = response.data[this.id];
-    });
+    // this.$watch(                           ********* first Method *********
+    //   () => this.$route.params,
+    //   () => {
+    //     this.getPostDetails();
+    //   },
+    // );
+    this.getPostDetails();
+  },
+  methods: {
+    getPostDetails() {
+      this.id = this.$route.params.id;
+      axios.get(`posts.json?id=${this.id}`).then((response) => {
+        this.post = response.data[this.id];
+      });
+    },
   },
 };
 </script>
