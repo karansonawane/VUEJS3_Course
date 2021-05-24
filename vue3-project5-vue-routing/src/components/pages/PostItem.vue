5c3445f6ba301e0eb7315cea5393a79564a76daf
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-24 13:49:18 +0530

    PostItem file uploaded

---
+++ vue3-project5-vue-routing/src/components/pages/PostItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>Post Details</h3>
+    <div>
+      <div>Id: {{ id }}</div>
+      <div>Title: {{ post.title }}</div>
+      <div>Description: {{ post.description }}</div>
+    </div>
+    <div>
+        <router-link class="btn btn-dark" to="/posts">Back</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      post: "",
+      id: "",
+    };
+  },
+  created() {
+    this.id = this.$route.params.id;
+    axios.get(`posts.json?id=${this.id}`).then((response) => {
+      this.post = response.data[this.id];
+    });
+  },
+};
+</script>
