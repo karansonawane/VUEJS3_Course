d85fff164b68cf6e7fa39fbb0f4e22951c4ecc59
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-25 11:41:10 +0530

    Not Found file Uploaded

---
+++ vue3-project5-vue-routing/src/components/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        Sorry... The Page you want is not found!
+        <div>
+            <router-link to="/">Go to Home Page</router-link>
+        </div>
+    </div>
+</template>
