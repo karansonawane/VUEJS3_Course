62936060c52046ad9ea3b1517e073141a6966e18
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-25 11:39:12 +0530

    Update Posts.vue

--- vue3-project5-vue-routing/src/components/pages/Posts.vue
+++ vue3-project5-vue-routing/src/components/pages/Posts.vue
@@ -1,53 +1,57 @@
 <template>
+  <div class="row">
+    <div class="col-md-8"></div>
     <div>
-        <h3>Post Details</h3>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th> Id </th>
-                    <th> Title </th>
-                    <th> Description </th>
-                    <th>
-                        Action
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for:="post in postDetails" :key="post[id]">
-                    <td>{{post.id}}</td>
-                    <td>{{post.title}}</td>
-                    <td>{{post.description}}</td>
-                    <td>
-                        <router-link :to="`/posts/${post.id}`" class="btn btn-primary">Show Details</router-link>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+      <h3>Post Details</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for:="post in postDetails" :key="post[id]">
+            <td>{{ post.id }}</td>
+            <td>{{ post.title }}</td>
+            <td>{{ post.description }}</td>
+            <td>
+              <router-link :to="`/posts/${post.id}`" class="btn btn-primary"
+                >Show Details</router-link
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+    <div class="col-md-4">
+        <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios';
-    export default {
-        name: 'Posts',
-        data() {
-            return {
-                postDetails: [],
-            };
-        },
-        mounted() {
-            axios
-                .get(`posts.json`)
-                .then((response) => {
-                    this.formatPosts(response.data);
-                });
-        },
-        methods: {
-            formatPosts(postsData) {
-                for (let key in postsData) {
-                    this.postDetails.push({ ...postsData[key], id: key });
-                }
-            }
-        }
-    }
+import axios from "axios";
+export default {
+  name: "Posts",
+  data() {
+    return {
+      postDetails: [],
+    };
+  },
+  mounted() {
+    axios.get(`posts.json`).then((response) => {
+      this.formatPosts(response.data);
+    });
+  },
+  methods: {
+    formatPosts(postsData) {
+      for (let key in postsData) {
+        this.postDetails.push({ ...postsData[key], id: key });
+      }
+    },
+  },
+};
 </script>
