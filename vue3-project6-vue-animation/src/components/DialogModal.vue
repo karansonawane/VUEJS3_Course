b9b17b62a94888b3b38a0b892a4e9f087ae17f51
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-26 20:40:23 +0530

    Dialog Modal Vue file uploaded

---
+++ vue3-project6-vue-animation/src/components/DialogModal.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <transition name="backdrop">
+      <div class="backdrop" v-if="open"></div>
+    </transition>
+    <transition>
+      <div class="dialog" v-if="open">
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["open"],
+};
+</script>
+
+<style scoped>
+.backdrop {
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+}
+
+.dialog {
+  position: fixed;
+  background: #fff;
+  top: 20%;
+  width: 60vw;
+  left: 20vw;
+  padding: 20px;
+}
+
+/* enter */
+
+.v-enter-from {
+  transform: translateY(-200px);
+}
+
+.v-enter-to {
+  transform: translateY(0px);
+}
+
+.v-enter-active {
+  transition: transform 0.5s;
+}
+
+/* leave */
+
+.v-leave-from {
+  transform: translateY(0px);
+}
+
+.v-leave-to {
+  transform: translateY(-200px);
+}
+
+.v-leave-active {
+  transition: transform 0.5s;
+}
+
+/* backdrop enter */
+
+.backdrop-enter-from,
+.backdrop-leave-to {
+  opacity: 0;
+}
+
+.backdrop-enter-to,
+.backdrop-leave-from {
+  opacity: 1;
+}
+.backdrop-enter-active,
+.backdrop-leave-active {
+  transition: opacity 1s;
+}
+</style>
