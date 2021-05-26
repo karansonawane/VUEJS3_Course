37f42329cc8e0d6d8b442355833c0edb36df2252
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-26 20:38:47 +0530

    Update App.vue

--- vue3-project6-vue-animation/src/App.vue
+++ vue3-project6-vue-animation/src/App.vue
@@ -14,28 +14,52 @@
         <div class="my-3">
           <animate-paragraph></animate-paragraph>
         </div>
+        <div class="my-3">
+            <dialog-modal :open="openDialog">
+              <div>This is Dialog Modal</div>
+              <div class="my-3">
+                <button
+                  class="btn btn-success"
+                  @click.prevent="openDialog = false"
+                >
+                  Okay!
+                </button>
+              </div>
+            </dialog-modal>
+          <div>
+            <button class="btn btn-dark" @click.prevent="onOpenDialog()">
+              Open Dialog Modal
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import TheParagraph from './components/TheParagraph.vue';
-import AnimateParagraph from './components/AnimateParagraph.vue';
+import TheParagraph from "./components/TheParagraph.vue";
+import AnimateParagraph from "./components/AnimateParagraph.vue";
+import DialogModal from "./components/DialogModal.vue";
 export default {
-  components: { TheParagraph, AnimateParagraph },
+  components: { TheParagraph, AnimateParagraph, DialogModal },
   data() {
     return {
       animate: false,
-    }
+      openDialog: false,
+    };
   },
   component: {
     TheParagraph,
     AnimateParagraph,
+    DialogModal,
   },
   methods: {
     onAnimateClick() {
       this.animate = true;
+    },
+    onOpenDialog() {
+      this.openDialog = true;
     },
   },
 };
@@ -49,18 +73,18 @@ export default {
   background: rebeccapurple;
   /* transition: transform 0.5s; */
 }
-.animate{
+.animate {
   /* transform: translateX(150px); */
   animation: animate-box 1s;
 }
-@keyframes animate-box{
-  0%{
+@keyframes animate-box {
+  0% {
     transform: translateX(0px) scaleX(1);
   }
-  50%{
+  50% {
     transform: translateX(100px) scaleX(1.5);
   }
-  100%{
+  100% {
     transform: translateX(200px) scaleX(1);
   }
 }
