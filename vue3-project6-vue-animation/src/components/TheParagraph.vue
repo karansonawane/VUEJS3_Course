c41385c86e760d674c036e7d3fe0cf309c4d5481
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-26 13:17:17 +0530

    The Paragraph file uploaded

---
+++ vue3-project6-vue-animation/src/components/TheParagraph.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="text-center">
+    <transition>
+      <p v-if="showParagraph">Hai Karan</p>
+    </transition>
+    <button class="btn btn-primary" @click.prevent="onToggle()">
+      Toggle Text
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showParagraph: true,
+    };
+  },
+  methods: {
+    onToggle() {
+      this.showParagraph = !this.showParagraph;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.v-enter-from{
+    opacity: 0;
+    transform: translateY(-10px);
+}
+
+.v-enter-to{
+    opacity: 1;
+    transform: translateY(0px);
+}
+
+.v-enter-active {
+    transition: all 0.3s;
+}
+.v-leave-from{
+    opacity: 1;
+    transform: translateY(0px);
+}
+.v-leave-to {
+    opacity: 0;
+    transform: translateY(-10px);
+}
+.v-leave-active {
+    transition: all 0.3s;
+}
+</style>
