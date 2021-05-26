98e259a99220b3411f7a5336f89547c12f845bca
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-26 12:51:46 +0530

    Update App.vue

--- vue3-project6-vue-animation/src/App.vue
+++ vue3-project6-vue-animation/src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box" :class="{ animate: animate }"></div>
+        <div class="mt-3">
+          <button class="btn btn-primary" @click.prevent="onAnimateClick()">
+            Click me
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      animate: false,
+    }
+  },
+  methods: {
+    onAnimateClick() {
+      this.animate = true;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.box {
+  width: 100px;
+  height: 100px;
+  border: 1px solid rebeccapurple;
+  background: rebeccapurple;
+  /* transition: transform 0.5s; */
+}
+.animate{
+  /* transform: translateX(150px); */
+  animation: animate-box 1s;
+}
+@keyframes animate-box{
+  0%{
+    transform: translateX(0px) scaleX(1);
+  }
+  50%{
+    transform: translateX(100px) scaleX(1.5);
+  }
+  100%{
+    transform: translateX(200px) scaleX(1);
+  }
 }
 </style>
