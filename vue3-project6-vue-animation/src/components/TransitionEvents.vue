e1e9d4b164b2d7a8d255180e7f0d50ed9fe37888
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-28 19:41:08 +0530

    Transition Events File Added.

---
+++ vue3-project6-vue-animation/src/components/TransitionEvents.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="text-primary py-4 text-center" style="border: 1px solid #000">
+    <transition name="button" mode="out-in"
+    @before-enter="beforeEnter"
+    @after-enter="afterEnter"
+    @enter ="enter"
+    @before-leave="beforeLeave"
+    @after-leave="afterLeave"
+    @leave ="leave">
+      <button
+        v-if="!showOnButton"
+        class="btn btn-danger"
+        @click.prevent="onShowButton()"
+      >
+        Off
+      </button>
+      <button v-else class="btn btn-warning" @click.prevent="onShowButton()">
+        On
+      </button>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showOnButton: true,
+    };
+  },
+  methods: {
+    onShowButton() {
+      this.showOnButton = !this.showOnButton;
+    },
+    beforeEnter() {
+        console.log('before enter');
+    },
+    afterEnter() {
+        console.log('after enter');
+    },
+    enter() {
+        console.log('enter');
+    },
+
+    beforeLeave() {
+        console.log('before leave');
+    },
+    afterLeave() {
+        console.log('after leave');
+    },
+    leave() {
+        console.log('leave');
+    }
+  },
+};
+</script>
+
+<style scoped>
+.button-enter-from,
+.button-leave-to {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.button-enter-to,
+.button-leave-from {
+  opacity: 1;
+  transform: translateX(0px);
+}
+
+.button-enter-active .button-leave-active {
+  transition: all 0.5s;
+}
+</style>
