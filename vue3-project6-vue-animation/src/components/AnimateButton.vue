c4a87c3a72416280ba5316c9692119f162b767c1
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-28 19:08:48 +0530

    Animate Button File Added

---
+++ vue3-project6-vue-animation/src/components/AnimateButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="text-primary py-4 text-center" style="border: 1px solid #000">
+    <transition name="button" mode="out-in">
+      <button
+        v-if="!showOnButton"
+        class="btn btn-danger"
+        @click.prevent="onShowButton()"
+      >
+        Off
+      </button>
+      <button v-else class="btn btn-warning" @click.prevent="onShowButton()">
+        On
+      </button>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showOnButton: true,
+    };
+  },
+  methods: {
+    onShowButton() {
+      this.showOnButton = !this.showOnButton;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.button-enter-from,
+.button-leave-to {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.button-enter-to,
+.button-leave-from {
+  opacity: 1;
+  transform: translateX(0px);
+}
+
+.button-enter-active .button-leave-active {
+  transition: all 0.5s;
+}
+</style>
