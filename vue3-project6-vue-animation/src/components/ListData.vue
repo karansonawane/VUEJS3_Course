eaba89763c69be7c2bf0cdb51d92e16d9e585e92
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-29 11:36:02 +0530

    Update ListData.vue

--- vue3-project6-vue-animation/src/components/ListData.vue
+++ vue3-project6-vue-animation/src/components/ListData.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <div class="my-3">
-      <span class="mx-1" v-for="number in numbers" :key="number">{{
-        number
-      }}</span>
-    </div>
-    <div>
+    <div class="mb-3">
       <button @click.prevent="onAdd()">Add</button>
       <button @click.prevent="onRemove()">Remove</button>
       <button @click.prevent="onShuffle()">Shuffle</button>
     </div>
+
+    <transition-group tag="div" name="list">
+      <span class="mx-1 list-item" v-for="number in numbers" :key="number">{{
+        number
+      }}</span>
+    </transition-group>
   </div>
 </template>
 
@@ -18,7 +19,7 @@ export default {
   data() {
     return {
       numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      nextNum: 0,
+      nextNum: 10,
     };
   },
   methods: {
@@ -32,9 +33,35 @@ export default {
         (number) => number !== this.numbers[randomIndex]
       );
     },
-    onShuffle(){
-        this.numbers.reverse();
-    }
+    onShuffle() {
+      this.numbers.reverse();
+    },
   },
 };
 </script>
+
+<style scoped>
+
+.list-item{
+    display: inline-block;
+}
+
+.list-enter-from,
+.list-leave-to{
+    opacity: 0;
+    transform: translateY(30px);
+}
+
+.list-enter-to,
+.list-leave-from{
+    opacity: 1;
+    transform: translateY(0px);
+}
+
+.list-enter-active{
+    transition: all 1s ease-in;
+}
+.list-leave-active{
+    transition: all 1s ease-out;
+}
+</style>
