90cdf8eb22405e4b820d370b07e48aa52a3de6ea
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-30 12:25:08 +0530

    Update main.js

--- vue-vuexcourse/src/main.js
+++ vue-vuexcourse/src/main.js
@@ -6,11 +6,22 @@ const store = createStore({
     state() {
         return {
             count: 0,
+            todos: [
+                { id: 1, text: 'wakeup early', done: true },
+                { id: 2, text: 'do breakfast', done: true },
+                { id: 3, text: 'go to school', done: false },
+                { id: 4, text: 'sleep well', done: false },
+            ],
         }
     },
     mutations: {
         increment(state, payload) {
             state.count = state.count + payload.value;
+        }
+    },
+    getters: {
+        doneTodosListCount(state) {
+            return state.todos.filter((todo) => todo.done).length;
         }
     }
 })
