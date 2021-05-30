dfd793c33d1c4df9e94117a7f73ad020c16b830f
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-30 12:25:39 +0530

    Update App.vue

--- vue-vuexcourse/src/App.vue
+++ vue-vuexcourse/src/App.vue
@@ -1,36 +1,48 @@
 <template>
-<div>
-<counter></counter>
-<div>
-  <h3>Vuex Counter</h3>
-  {{ count }}
-</div>
-<button @click.prevent="onIncrement()">Increment</button>
-</div>
+  <div>
+    <counter></counter>
+    <div>
+      <h3>Vuex Counter</h3>
+      {{ count }}
+    </div>
+    <button @click.prevent="onIncrement()">Increment</button>
+    <div>
+      <todos-list></todos-list>
+    </div>
+    <div>Todos List complete: {{ doneListCount }}</div>
+  </div>
 </template>
 
 <script>
-import Counter from './components/Counter.vue';
+import Counter from "./components/Counter.vue";
+import TodosList from "./components/TodosList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   computed: {
     count() {
       return this.$store.state.count;
-    }
+    },
+    doneListCount() {
+      return this.$store.getters.doneTodosListCount;
+    },
   },
   components: {
     Counter,
+    TodosList,
   },
   methods: {
     onIncrement() {
       return this.$store.commit({
-        type: 'increment',
+        type: "increment",
         value: 4,
       });
-    }
-  }
-}
+    },
+    // getDoneListCount() {
+    //   return this.$store.state.todos.filter((todo) => todo.done).length;
+    // },  ***************** Insted of this method, we used above computed property "doneListCount()""  *******************
+  },
+};
 </script>
 
 <style>
