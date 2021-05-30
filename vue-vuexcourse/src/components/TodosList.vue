708eb449ab8928e7227d89286b56d114b2420fac
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-30 12:26:40 +0530

    Todos List file added.

---
+++ vue-vuexcourse/src/components/TodosList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div style="text-align: left">
+        <h3>Todos List (Done: {{ doneListCount }})</h3>
+        <ul>
+            <li v-for="todo in todosList" :key="todo.id">{{ todo.text }} - ({{ todo.done ? 'Done' : 'Not Done' }})</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+           
+        }
+    },
+    computed: {
+        todosList() {
+            return this.$store.state.todos;
+        },
+        doneListCount() {
+            return this.$store.getters.doneTodosListCount;
+        }
+    },
+    methods: {
+        // getDoneListCount() {
+        //     return this.todosList.filter((todo) => todo.done).length;
+        // } ***************** Insted of this method, we used above computed property "doneListCount()""  *******************
+    }
+}
+</script>
