726b220ece01cd106605e7a49ce69f7feee58e21
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-31 11:43:51 +0530

    Update Counter.vue

--- vue-vuexcourse/src/components/Counter.vue
+++ vue-vuexcourse/src/components/Counter.vue
@@ -1,32 +1,30 @@
 <template>
-    <div>
-        <div>{{ count }}</div>
-        <button @click.prevent="onIncrement()">Increment</button>
-    </div>
+  <div>
+    <div>{{ count }}</div>
+    <button @click.prevent="onIncrement()">Increment</button>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            
-        }
+  data() {
+    return {};
+  },
+  methods: {
+    onIncrement() {
+        this.$store.dispatch({
+            type: 'increment',
+            value: 3,
+        });
+    } /* ****************** 1st method **************** */
+    // onIncrement() {
+    //   this.$store.commit("increment", { value: 2 });
+    // } /* *********** 2nd Method ************* */,
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
     },
-    methods: {
-        // onIncrement() {
-        //     this.$store.commit({
-        //         type: 'increment',
-        //         value: 3,
-        //     });
-        // } ****************** 1st method ****************
-        onIncrement() {
-            this.$store.commit('increment', { value: 2 });
-        } /* *********** 2nd Method ************* */
-    },
-    computed: {
-        count() {
-            return this.$store.state.count;
-        }
-    },
-}
+  },
+};
 </script>
