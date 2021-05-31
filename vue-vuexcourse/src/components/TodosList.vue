ecee55bf3e1633e236fef30de476f625f58360b4
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-05-31 11:18:29 +0530

    Update TodosList.vue

--- vue-vuexcourse/src/components/TodosList.vue
+++ vue-vuexcourse/src/components/TodosList.vue
@@ -2,30 +2,51 @@
     <div style="text-align: left">
         <h3>Todos List (Done: {{ doneListCount }})</h3>
         <ul>
-            <li v-for="todo in todosList" :key="todo.id">{{ todo.text }} - ({{ todo.done ? 'Done' : 'Not Done' }})</li>
+            <li v-for="todo in todosList" :key="todo.id">
+                <a href="#" @click.prevent="onTodoSelected(todo.id)"> {{ todo.text }} - ({{ todo.done ? 'Done' : 'Not Done' }})</a>
+            </li>
+        </ul>
+        <div v-if="todo">
+            <h3>Selected Todo</h3>
+            <div>Id: {{todo.id}}</div>
+            <div>Text: {{todo.text}}</div>
+            <div>Done: {{todo.done}}</div>
+        </div>
+        <h3>Completed Todos</h3>
+        <ul>
+             <li v-for="todo in doneTodos" :key="todo.id">{{ todo.text }} - ({{ todo.done ? 'Done' : 'Not Done' }})</li>
         </ul>
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-           
-        }
+  data() {
+    return {
+      selectedId: 0,
+    };
+  },
+  computed: {
+    todosList() {
+      return this.$store.state.todos;
     },
-    computed: {
-        todosList() {
-            return this.$store.state.todos;
-        },
-        doneListCount() {
-            return this.$store.getters.doneTodosListCount;
-        }
+    doneListCount() {
+      return this.$store.getters.doneTodosListCount;
     },
-    methods: {
-        // getDoneListCount() {
-        //     return this.todosList.filter((todo) => todo.done).length;
-        // } ***************** Insted of this method, we used above computed property "doneListCount()""  *******************
-    }
-}
+    doneTodos() {
+      return this.$store.getters.doneTodos;
+    },
+    todo() {
+      return this.$store.getters.getTodoById(this.selectedId);
+    },
+  },
+  methods: {
+    // getDoneListCount() {
+    //     return this.todosList.filter((todo) => todo.done).length;
+    // } ***************** Insted of this method, we used above computed property "doneListCount()""  *******************
+    onTodoSelected(id) {
+      this.selectedId = id;
+    },
+  },
+};
 </script>
