923231538a6fe562bdfb80446932d68f38e16078
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-04 13:01:03 +0530

    Vue Authentication new vue project added.

---
+++ vue3-project7-vue-authentication/src/pages/Signup.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <div class="col-md-6 offset-md-3">
+      <div>
+        <div>
+          <h3>Signup</h3>
+          <hr />
+        </div>
+        <form action="">
+          <div class="form-group">
+            <label for="">Email</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label for="">Password</label>
+            <input type="password" class="form-control" />
+          </div>
+          <div class="my-3">
+            <button type="submit" class="btn btn-primary">Signup</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
