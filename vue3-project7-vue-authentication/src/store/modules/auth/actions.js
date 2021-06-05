26e866b4b7241f91d897d8aaac0786df47a211aa
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-05 20:01:18 +0530

    Update actions.js

--- vue3-project7-vue-authentication/src/store/modules/auth/actions.js
+++ vue3-project7-vue-authentication/src/store/modules/auth/actions.js
@@ -1,1 +1,18 @@
-export default {};
+import axios from 'axios';
+import { SIGNUP_ACTION } from '../../storeconstants';
+
+export default {
+    async [SIGNUP_ACTION](_, payload) {
+        let postData = {
+            email: payload.email,
+            password: payload.password,
+            returnSecureToken: true,
+        };
+        let response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC01b23UF7AzSNlsY_q8DH8ZHc3ucv6g5k`,
+            postData,
+        );
+
+        console.log(response);
+    },
+
+};
