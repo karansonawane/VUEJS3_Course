2c10d72aecf9e5012d65489a3ed535013d0dcded
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-07 12:33:25 +0530

    Update SignupValidations.js

--- vue3-project7-vue-authentication/src/services/SignupValidations.js
+++ vue3-project7-vue-authentication/src/services/SignupValidations.js
@@ -11,14 +11,20 @@ export default class SignupValidations {
         let errors = [];
         //email validations
 
-        if(!validations.checkEmail(this.email)) {
-            errors ['email'] = 'Invalid Email';
+        if (!validations.checkEmail(this.email)) {
+            errors['email'] = 'Invalid Email';
         }
 
         //password validations
-        if(!validations.minLength(this.password, 6)) {
+        if (!validations.minLength(this.password, 6)) {
             errors['password'] = 'Password should be of 6 characters';
         }
         return errors;
+    }
+    static getErrorMessageFromCode(errorCode) {
+        switch (errorCode) {
+            case 'EMAIL_EXISTS':
+                return 'Email already exists';
+        }
     }
 }
