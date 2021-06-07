e7f7a1b70252d0cc7f5d6a260f18cdc03d034d42
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-07 20:21:29 +0530

    Update actions.js

--- vue3-project7-vue-authentication/src/store/modules/auth/actions.js
+++ vue3-project7-vue-authentication/src/store/modules/auth/actions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import SignupValidations from '../../../services/SignupValidations';
-import { SET_USER_TOKEN_DATA_MUTATION, SIGNUP_ACTION } from '../../storeconstants';
+import { LOADING_SPINNER_SHOW_MUTATION, SET_USER_TOKEN_DATA_MUTATION, SIGNUP_ACTION } from '../../storeconstants';
 
 export default {
     async [SIGNUP_ACTION](context, payload) {
@@ -10,14 +10,20 @@ export default {
             returnSecureToken: true,
         };
         let response = '';
+
+        context.commit(LOADING_SPINNER_SHOW_MUTATION, true, { root: true });
+
         try {
             response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC01b23UF7AzSNlsY_q8DH8ZHc3ucv6g5k`,
                 postData,
             );
         } catch (err) {
+            context.commit(LOADING_SPINNER_SHOW_MUTATION, false, { root: true });
             let errorMessage = SignupValidations.getErrorMessageFromCode(err.response.data.error.errors[0].message,);
             throw errorMessage;
         }
+
+        context.commit(LOADING_SPINNER_SHOW_MUTATION, false, { root: true });
 
         if (response.status === 200) {
             context.commit(SET_USER_TOKEN_DATA_MUTATION), {
