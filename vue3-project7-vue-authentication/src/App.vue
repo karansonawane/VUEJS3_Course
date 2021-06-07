fda7ca3c49ebb60cca91e10f6c7743577b49de67
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-07 20:18:42 +0530

    Update App.vue

--- vue3-project7-vue-authentication/src/App.vue
+++ vue3-project7-vue-authentication/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <TheNavigation />
+    <the-loader v-if="showLoading"></the-loader>
     <div class="container">
       <div class="row">
         <div class="col-md-12">
@@ -15,10 +16,18 @@
 
 <script>
 import TheNavigation from "./components/TheNavigation.vue";
+import TheLoader from "./components/TheLoader.vue";
+import { mapState } from "vuex";
 export default {
   name: "App",
+  computed: {
+    ...mapState({
+      showLoading: (state) => state.showLoading,
+    }),
+  },
   components: {
     TheNavigation,
+    TheLoader,
   },
 };
 </script>
