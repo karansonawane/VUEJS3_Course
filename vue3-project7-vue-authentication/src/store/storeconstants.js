c44846d7734f67e7f016260de11dd486e2e09bfe
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-10 13:49:28 +0530

    Update storeconstants.js

--- vue3-project7-vue-authentication/src/store/storeconstants.js
+++ vue3-project7-vue-authentication/src/store/storeconstants.js
@@ -2,3 +2,4 @@ export const SIGNUP_ACTION = '[actions] signup user';
 export const LOGIN_ACTION = '[actions] login user';
 export const SET_USER_TOKEN_DATA_MUTATION = '[mutations] set user token';
 export const LOADING_SPINNER_SHOW_MUTATION = '[mutations] show loading spinner';
+export const GET_USER_TOKEN_GETTER = '[getters] auth user token';
