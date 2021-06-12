3eb9d9911204198e784df654b1f370dc8572edea
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-12 17:55:10 +0530

    Update router.js

--- vue3-project7-vue-authentication/src/router.js
+++ vue3-project7-vue-authentication/src/router.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from './pages/Home.vue';
 import Login from './pages/Login.vue';
 import Signup from './pages/Signup.vue';
 import Post from './pages/Post.vue';
 
 const routes = [
+    {
+        path: '', component: Home
+    },
     {
         path: '/login', component: Login
     },
