df3f72db44e41e0e65163b1c2b5b0282a7658bf5
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-12 17:55:48 +0530

    Update getters.js

--- vue3-project7-vue-authentication/src/store/modules/auth/getters.js
+++ vue3-project7-vue-authentication/src/store/modules/auth/getters.js
@@ -1,7 +1,10 @@
-import { GET_USER_TOKEN_GETTER } from "../../storeconstants";
+import { GET_USER_TOKEN_GETTER, IS_USER_AUTHENTICATE_GETTERS } from "../../storeconstants";
 
 export default {
     [GET_USER_TOKEN_GETTER]: (state) => {
         return state.token;
     }, 
+    [IS_USER_AUTHENTICATE_GETTERS](state) {
+        return !!state.token;
+    },
 };
