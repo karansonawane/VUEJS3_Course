fc9d421e85e8e25cb7d70fb371c16b672c4bee45
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-12 17:57:58 +0530

    Home vue file added

---
+++ vue3-project7-vue-authentication/src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>Home Page</div>
+        <div>
+            <router-link to="/posts">Go to posts page</router-link>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
