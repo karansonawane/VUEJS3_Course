5708857df2d350df52cbce3bfa7c925b19512578
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-12 17:54:31 +0530

    Update TheNavigation.vue

--- vue3-project7-vue-authentication/src/components/TheNavigation.vue
+++ vue3-project7-vue-authentication/src/components/TheNavigation.vue
@@ -15,14 +15,17 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item">
+          <li class="nav-item" v-if="isAuthenticated">
             <router-link class="nav-link" to="/">Home</router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item" v-if="!isAuthenticated">
             <router-link class="nav-link" to="/login">Login</router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item" v-if="!isAuthenticated">
             <router-link class="nav-link" to="/signup">Signup</router-link>
+          </li>
+          <li class="nav-item" v-if="isAuthenticated">
+            <router-link class="nav-link" to="/posts">Posts</router-link>
           </li>
         </ul>
       </div>
@@ -31,5 +34,13 @@
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+import { IS_USER_AUTHENTICATE_GETTERS } from "../store/storeconstants";
+export default {
+  computed: {
+    ...mapGetters("auth", {
+      isAuthenticated: IS_USER_AUTHENTICATE_GETTERS,
+    }),
+  },
+};
 </script>
