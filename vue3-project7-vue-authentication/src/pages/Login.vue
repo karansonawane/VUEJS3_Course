b6548a3cf86d29d8d3cabcdd46425a943fe7e16f
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-14 11:34:48 +0530

    Update Login.vue

--- vue3-project7-vue-authentication/src/pages/Login.vue
+++ vue3-project7-vue-authentication/src/pages/Login.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="row">
-    <div class="col-md-6 offset-md-3">
-      <div>
-        <div>
-          <h3>Login</h3>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 offset-3">
+          <div>
+            <h3>Login</h3>
+          </div>
           <hr />
-        </div>
-        <div class="alert alert-danger" v-if="error">{{ error }}</div>
-        <form @submit.prevent="onLogin()">
-          <div class="form-group">
-            <label for="">Email</label>
-            <input type="text" class="form-control" v-model.trim="email" />
-            <div class="error" v-if="errors.email">{{ errors.email }}</div>
-          </div>
-          <div class="form-group">
-            <label for="">Password</label>
-            <input
-              type="password"
-              class="form-control"
-              v-model.trim="password"
-            />
-            <div class="error" v-if="errors.password">
-              {{ errors.password }}
+          <div class="alert alert-danger" v-if="error">{{ error }}</div>
+          <form @submit.prevent="onLogin()">
+            <div class="form-group">
+              <label for="">Email</label>
+              <input type="text" class="form-control" v-model.trim="email" />
+              <div class="error" v-if="errors.email">{{ errors.email }}</div>
             </div>
-          </div>
-          <div class="my-3">
-            <button type="submit" class="btn btn-primary">Login</button>
-          </div>
-        </form>
+            <div class="form-group mt-4">
+              <label for="">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                v-model.trim="password"
+              />
+              <div class="error" v-if="errors.password">
+                {{ errors.password }}
+              </div>
+            </div>
+            <div class="my-3">
+              <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
