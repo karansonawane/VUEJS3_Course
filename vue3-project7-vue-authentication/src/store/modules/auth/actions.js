de11ecba8a8c339befb159cfe3966b3e6d977cb9
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-06-14 11:33:06 +0530

    Update actions.js

--- vue3-project7-vue-authentication/src/store/modules/auth/actions.js
+++ vue3-project7-vue-authentication/src/store/modules/auth/actions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import SignupValidations from '../../../services/SignupValidations';
-import { LOADING_SPINNER_SHOW_MUTATION, LOGIN_ACTION, LOGOUT_ACTION, SET_USER_TOKEN_DATA_MUTATION, SIGNUP_ACTION } from '../../storeconstants';
+import { AUTH_ACTION, AUTO_LOGIN_ACTION, LOADING_SPINNER_SHOW_MUTATION, LOGIN_ACTION, LOGOUT_ACTION, SET_USER_TOKEN_DATA_MUTATION, SIGNUP_ACTION } from '../../storeconstants';
 
 export default {
 
@@ -12,9 +12,33 @@ export default {
             refreshToken: null,
             userId: null,
         });
+        localStorage.removeItem('userData');
     },
 
     async [LOGIN_ACTION](context, payload) {
+        return context.dispatch(AUTH_ACTION, {
+            ...payload,
+            url: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC01b23UF7AzSNlsY_q8DH8ZHc3ucv6g5k`
+        })
+    },
+
+    async [SIGNUP_ACTION](context, payload) {
+        return context.dispatch(AUTH_ACTION, {
+            ...payload,
+            url: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC01b23UF7AzSNlsY_q8DH8ZHc3ucv6g5k`
+        });
+    },
+
+    [AUTO_LOGIN_ACTION](context) {
+        let userData = localStorage.getItem('userData');
+        if (userData) {
+            context.commit(SET_USER_TOKEN_DATA_MUTATION,
+                JSON.parse(userData),
+            );
+        }
+    },
+
+    async [AUTH_ACTION](context, payload) {
         let postData = {
             email: payload.email,
             password: payload.password,
@@ -23,9 +47,7 @@ export default {
         let response = '';
 
         try {
-            response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC01b23UF7AzSNlsY_q8DH8ZHc3ucv6g5k`,
-                postData,
-            );
+            response = await axios.post(payload.url, postData);
         } catch (err) {
             context.commit(LOADING_SPINNER_SHOW_MUTATION, false, { root: true });
             let errorMessage = SignupValidations.getErrorMessageFromCode(err.response.data.error.errors[0].message,);
@@ -33,46 +55,15 @@ export default {
         }
 
         if (response.status === 200) {
-            context.commit(SET_USER_TOKEN_DATA_MUTATION, {
+            let tokenData = {
                 email: response.data.email,
                 token: response.data.idToken,
                 expiresIn: response.data.expiresIn,
                 refreshToken: response.data.refreshToken,
                 userId: response.data.localId,
-            });
-        }
-    },
-
-    async [SIGNUP_ACTION](context, payload) {
-        let postData = {
-            email: payload.email,
-            password: payload.password,
-            returnSecureToken: true,
-        };
-        let response = '';
-
-        // context.commit(LOADING_SPINNER_SHOW_MUTATION, true, { root: true });
-
-        try {
-            response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC01b23UF7AzSNlsY_q8DH8ZHc3ucv6g5k`,
-                postData,
-            );
-        } catch (err) {
-            // context.commit(LOADING_SPINNER_SHOW_MUTATION, false, { root: true });
-            let errorMessage = SignupValidations.getErrorMessageFromCode(err.response.data.error.errors[0].message,);
-            throw errorMessage;
-        }
-
-        // context.commit(LOADING_SPINNER_SHOW_MUTATION, false, { root: true });
-
-        if (response.status === 200) {
-            context.commit(SET_USER_TOKEN_DATA_MUTATION, {
-                email: response.data.email,
-                token: response.data.idToken,
-                expiresIn: response.data.expiresIn,
-                refreshToken: response.data.refreshToken,
-                userId: response.data.localId,
-            })
+            }
+            localStorage.setItem('userData', JSON.stringify(tokenData));
+            context.commit(SET_USER_TOKEN_DATA_MUTATION, tokenData);
         }
     },
 
