6c77e35d8c558d11de8ed130a9f9d9dc38a214e5
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-07-05 12:15:50 +0530

    Update App.vue

--- vue3-project8-compositionapi/src/App.vue
+++ vue3-project8-compositionapi/src/App.vue
@@ -1,25 +1,39 @@
 <template>
   <div>
     <div>My Full Name is : {{userName}}</div>
+
+    <h3>User Details</h3>
+    <div>Name : {{userDetails.name}}</div>
+    <div>Age : {{userDetails.age}}</div>
   </div>
 </template>
 
 <script>
-  import {ref} from 'vue';
-    export default {
+  import { ref } from 'vue';
+  export default {
 
-      setup() {
-        let name = ref('Karan Sonawane');
+    setup() {
+      let name = ref('Karan Sonawane');
 
-        setTimeout(() => {
-          console.log('executing time out in setup');
-          console.log(name);
-          name.value = 'Karan Dinkar Sonawane';
-        },3000);
-        return{
-          userName: name,
-        }
-      },
+      let userDetails = ref({
+        name: 'Karan',
+        age: 22,
+      });
+
+      setTimeout(() => {
+        console.log('executing time out in setup');
+        console.log(name);
+        name.value = 'Karan Dinkar Sonawane';
+        userDetails.value.name = 'Changed Name';
+        userDetails.value.age = 22;
+      }, 3000);
+      return {
+        userName: name,
+        name: userDetails.value.name,
+        age: userDetails.value.age,
+        userDetails: userDetails,
+      }
+    },
     // name: 'App',
     // data(){
     //   return{
@@ -38,7 +52,7 @@
 </script>
 
 <style>
-#app {
+  #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
